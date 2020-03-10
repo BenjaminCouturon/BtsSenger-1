dced841569ad47881992ab5bf11e853ee59881fa
benjamin.couturon <benji.couturon@netcourrier.com>
2020-03-10 16:41:34 +0100

    framgent

--- app/src/main/java/fr/lasalle/btssenger/presentation/FriendProfil.java
+++ app/src/main/java/fr/lasalle/btssenger/presentation/FriendProfil.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import fr.lasalle.btssenger.R;
+import fr.lasalle.btssenger.entity.User;
+import fr.lasalle.btssenger.service.OnCompleteListener;
 import fr.lasalle.btssenger.service.UsersService;
 
 public class FriendProfil extends AppCompatActivity {
+    private  UsersService usersService = new UsersService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,23 @@ public class FriendProfil extends AppCompatActivity {
 
 
         String profileId = getIntent().getStringExtra("PROFIL_ID");
-        
+        usersService.loadUserProfile(profileId, new OnCompleteListener<User>() {
+            @Override
+            public void onSuccess(User result) {
+                ((TextView)findViewById(R.id.account_activity_fullname)).setText(result.getName());
+                Picasso.get().load(result.getImage()).placeholder(R.drawable.ic_user).into((ImageView)findViewById(R.id.account_activity_avatar));
+            }
+
+
+            @Override
+            public void onError() {
+
+            }
+
+            @Override
+            public void onLoad(boolean loading) {
+
+            }
+        });
     }
 }
